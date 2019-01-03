c7055d915317907f4a9ea4dc3a6c05bdb814bd66
yukinekato <40753900+yukinekato@users.noreply.github.com>
2019-01-03 21:21:39 +0900

    Update shooting.js

--- shooting.js
+++ shooting.js
@@ -1,8 +1,12 @@
 "use strict"
 var canvas, ctx;
+var　ENEMIES=10;
 var img_player;
 var img_enemy;
 var player_x, player_y;
+var enemies_x = new Array(ENEMIES);
+var enemies_y = new Array(ENEMIES);
+
 
 window.onkeydown=function(e){
   var SPEED=2;
@@ -19,6 +23,9 @@ window.onkeydown=function(e){
  if(moved){
    ctx.clearRect(0,0,canvas.width,canvas.height);
    ctx.drawImage(img_player,player_x,player_y);
+   for(var i=0; i<ENEMIES; i++){
+     ctx.drawImage(img_enemy, enemies_x[i], enemies_y[i]);
+   }
  }
 };
 window.onload=function(){
@@ -28,9 +35,13 @@ window.onload=function(){
   img_enemy=document.getElementById('enemy');
   player_x=(canvas.width-player.width)/2;
   player_y=(canvas.height-player.height)-20;
+  for(var i=0; i<ENEMIES; i++){
+    enemies_x[i]=Math.random()*(canvas.width-img_enemy.width);
+    enemies_y[i]=Math.random()*(canvas.height-img_enemy.height);
+  }
   ctx.drawImage(img_player, player_x, player_y);
   for(var i=0; i<10; i++){
-  ctx.drawImage(img_enemy,Math.random()*(canvas.width-img_enemy.width),Math.random()*(canvas.height-img_enemy.height));
+  ctx.drawImage(img_enemy,enemies_x[i],enemies_y[i]);
 
  }
 };
