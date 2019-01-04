850582818b1e85130006f8af3002e41ac8ec61e3
yukinekato <40753900+yukinekato@users.noreply.github.com>
2019-01-04 11:07:55 +0900

    Update shooting.js

--- shooting.js
+++ shooting.js
@@ -1,12 +1,21 @@
 "use strict"
 var canvas, ctx;
+var FPS= 30;
+var MSPF=1000/FPS;
 var　ENEMIES=10;
 var img_player;
 var img_enemy;
 var player_x, player_y;
 var enemies_x = new Array(ENEMIES);
 var enemies_y = new Array(ENEMIES);
+var redraw =function(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.drawImage(img_player, player_x, player_y);
+  for(var i=0; i<ENEMIES; i++){
+    ctx.drawImage(img_enemy,enemies_x[i],enemies_y[i]);
 
+  }
+};
 
 window.onkeydown=function(e){
   var SPEED=2;
@@ -20,14 +29,20 @@ window.onkeydown=function(e){
    player_x-=SPEED;
    moved=true;
  }
- if(moved){
-   ctx.clearRect(0,0,canvas.width,canvas.height);
-   ctx.drawImage(img_player,player_x,player_y);
-   for(var i=0; i<ENEMIES; i++){
-     ctx.drawImage(img_enemy, enemies_x[i], enemies_y[i]);
-   }
- }
 };
+
+ 
+   var mainloop = function() {
+    var startTime = new Date();
+    redraw();
+  var deltaTime = (new Date()) - startTime;
+  var interval = MSPF - deltaTime;
+   if(interval > 0) {
+      setTimeout(mainloop, interval);
+    } else {
+      mainloop();
+    }
+  };
 window.onload=function(){
   canvas=document.getElementById('screen');
   ctx=canvas.getContext('2d');
@@ -38,10 +53,6 @@ window.onload=function(){
   for(var i=0; i<ENEMIES; i++){
     enemies_x[i]=Math.random()*(canvas.width-img_enemy.width);
     enemies_y[i]=Math.random()*(canvas.height-img_enemy.height);
-  }
-  ctx.drawImage(img_player, player_x, player_y);
-  for(var i=0; i<10; i++){
-  ctx.drawImage(img_enemy,enemies_x[i],enemies_y[i]);
-
- }
+   }
+   mainloop();
 };
